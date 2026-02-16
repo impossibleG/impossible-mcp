d167ca2f2c536f0a874d651296c622cd38372954
autoantohaki <mauricio.antohaki1@gmail.com>
2026-02-16 11:09:53 +0000

    feat: add index module

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export { loadConfig, type AppConfig } from "./config.js";
+export { buildServer, type ServerDependencies } from "./server.js";
+export { startHttp, type HttpRuntime } from "./http.js";
+export { startStdio } from "./stdio.js";
